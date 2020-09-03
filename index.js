a5fd579a19cbe66a99ccf9b7d7ab0cdd4bf987bb
Ilay Almalem <ilayalmalem@gmail.com>
2020-09-03 22:26:48 +0300

    v1.0.2 - Now the repeat directive is availble. not perfect, but availble

--- index.js
+++ index.js
@@ -61,31 +61,65 @@ class Lingo {
         })
     }
     setForLoops() {
+        // elements.forEach((element) => {
+        //     var statement = element.getAttribute('repeat')
+        //     var repeatorLiteral = statement.substr(0, statement.indexOf('in')).trim()
+        //     var object = statement.split('in').pop().trim();
+        //     var repeator = repeatorLiteral
+        //         try {
+        //             console.log(`${repeatorLiteral} in ${object}`)
+        //             for (repeatorLiteral in this.data[`${object}`]) {
+        //                 if(element.textContent.includes('{')) {
+        //                     let t = element.textContent.replace('{','')
+        //                     t = t.replace('}','')
+        //                     element.textContent = t
+
+        //                     var newLit = element.textContent.replace(`${repeator}`,`${this.data[`${object}`][`${repeatorLiteral}`]}`)
+        //                     var exec = eval(`this.data.${element.textContent}`)
+        //                     element.textContent = exec
+        //                     alert(exec)
+        //                 }
+        //             }
+        //         }
+        //         catch(e) {
+        //             console.log(e)
+        //         }
+        // })
+        // Get all elements with the repeat attribute, and foreach on them
         var elements = document.querySelectorAll('[repeat]')
-        elements.forEach((element) => {
+        elements.forEach((element,index) => {
+            // Get the data to be looped on
+            // This is the statement something like this: todo in todod; todo = repeator, object = this.data.todos
             var statement = element.getAttribute('repeat')
             var repeatorLiteral = statement.substr(0, statement.indexOf('in')).trim()
             var object = statement.split('in').pop().trim();
             var repeator = repeatorLiteral
-                try {
-                    console.log(`${repeatorLiteral} in ${object}`)
-                    for (repeatorLiteral in this.data[`${object}`]) {
-                        if(element.textContent.includes('{')) {
-                            let t = element.textContent.replace('{','')
-                            t = t.replace('}','')
-                            element.textContent = t
+            
+            element.textContent = this.sanitize(element.textContent)
+            var code = element.textContent
+            var encapsulated = code.split(/[{}]/);
 
-                            var newLit = element.textContent.replace(`${repeator}`,`${this.data[`${object}`][`${repeatorLiteral}`]}`)
-                            var exec = eval(`this.data.${element.textContent}`)
-                            element.textContent = exec
-                            alert(exec)
-                        }
-                    }
+            // remove whitespaces
+            encapsulated = encapsulated.filter(function (el) {
+                return el != '';
+            });
+            element.textContent = ''
+            // Loop over todos
+            this.data[object].forEach((el,index) => {
+                var todoItem = encapsulated[index]
+                code = this.sanitize(code)
+
+                for (const key in el) {
+                    element.innerHTML += '<div>' + eval(`this.data.${object}[${index}].${key}`) + '</div>'
                 }
-                catch(e) {
-                    console.log(e)
-                }
+            })
         })
+    }
+
+    sanitize(code) {
+        code = code.trim().replace('{','')
+        code = code.trim().replace('}','')
+        return code;
     }
 }
 
--- tester.js
+++ tester.js
@@ -1,7 +1,7 @@
 var lingo = new window.Lingo('#root',{
     todos: [
-        {name:'ss'},
-        'cxcx'
+        {name:'Item 1', desc:'item 33',porn:'pornhub'},
+        {name:'Item 2',desc: 'item 32',porn:'ss'}
     ]
 })
 
